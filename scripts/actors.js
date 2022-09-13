71bcb0787e9897fe1d3e87509ad29459c3a25cd2
Yelena Cox <yelenacox@gmail.com>
2022-09-13 10:16:36 -0500

    Added actors branch and blank function

---
+++ scripts/actors.js
@@ -0,0 +1,3 @@
+/*A TV watcher wants to see a list of actors. Just their names. When the actor name is clicked on, the show in which the actor is the star should be display in an alert message.*/
+
+const actors = () => {}
